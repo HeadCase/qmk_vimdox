85178ff1d54e85cc5567ebf38cc3d8a0d0bffab4
Mantas Jurkuvenas <MantasJurkuvenas@gmail.com>
2023-10-18 13:01:00 +0100

    added qwerty

--- keymaps/qwerty/keymap.c
+++ keymaps/qwerty/keymap.c
@@ -23,11 +23,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B,                                 KC_J, KC_L, KC_U, KC_Y, KC_SCLN,  MT(MOD_LGUI, KC_DEL),
+       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                            KC_Y, KC_U, KC_I, KC_O, KC_P,  MT(MOD_LGUI, KC_DEL),
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_ESC, KC_A, KC_R, KC_S, KC_T, KC_G,                                           KC_M, KC_N, KC_E, KC_I, KC_O, KC_UNDS,
+        KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G,                                           KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_UNDS,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_D, KC_V,                                          KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, TT(_NAV),
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                                          KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TT(_NAV),
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                         KC_SPC, KC_LALT, KC_LCTL,                     KC_BSPC, TT(_SYMB), KC_ENT
         //`--------------------------'  `--------------------------'
