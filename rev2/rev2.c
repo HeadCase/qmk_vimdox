4f3f97cb695f241ae075b34b5c802ce3f3482308
Mantas Jurkuvenas <MantasJurkuvenas@gmail.com>
2023-11-02 18:42:01 +0000

    ready for release

--- rev2/rev2.c
+++ rev2/rev2.c
@@ -1,19 +1,3 @@
-/* Copyright 2022 Leah Post <leah@splitkb.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "quantum.h"
 
 #ifdef SWAP_HANDS_ENABLE
@@ -30,6 +14,6 @@
     };
 // clang-format on
 #    ifdef ENCODER_MAP_ENABLE
-        const uint8_t PROGMEM encoder_hand_swap_config[NUM_ENCODERS] = {1,0};
+const uint8_t PROGMEM encoder_hand_swap_config[NUM_ENCODERS] = {1, 0};
 #    endif
 #endif
